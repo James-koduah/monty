ed61223c86ca7a8d15542734521dff959c48b781
James-Koduah <jameskoduah171@gmail.com>
2023-01-18 12:55:29 -0800

    implemened must_be valid

--- main.c
+++ main.c
@@ -2,9 +2,11 @@
 int stack[LIMIT] = {0};
 int push_stack = -1;
  
-void exit_str(char *mess)
+void exit_str(char *mess, char *ar)
 {
-	fprintf(stderr, "%s", mess);
+	if (ar == NULL)
+		ar = "";
+	fprintf(stderr, "%s%s\n", mess, ar);
 	exit(EXIT_FAILURE);
 }
 
@@ -18,7 +20,7 @@ int main(int argc, char *argv[])
 {
 	char line_buf[1000];
 	FILE *file_h;/*file handle*/
-	int i = 0;
+	int i = 0, match = 0, linecount = 0;
 	functions funcs[] = {
 		{"push", push},
 		{"pall", pall},
@@ -27,15 +29,13 @@ int main(int argc, char *argv[])
 	char *token, *token2;
 
 	if (argc < 2)
-		exit_str("USAGE: monty file\n");
+		exit_str("USAGE: monty file", NULL);
 
 	/* open the file */
 	file_h = fopen(argv[1], "r");
 	if (!file_h)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
+		exit_str("Error: Can't open file ", argv[1]);
+
 	while (fgets(line_buf, 1000, file_h))
 	{
 		token = strtok(line_buf, " \n");
@@ -43,10 +43,21 @@ int main(int argc, char *argv[])
 		while (funcs[i].op != NULL)
 		{
 			if (strcmp(token, funcs[i].op) == 0)
+			{
 				funcs[i].f(token2);
+				match = 1;
+			}
 			i++;
 		}
+		linecount++;
+		if (match == 0)
+		{
+			fprintf(stderr, "L%d: unknown instruction %s\n", linecount, token);
+			exit(EXIT_FAILURE);
+		}
+
 		i = 0;
+		match = 0;
 	}
 	fclose(file_h);
 	return (0);
